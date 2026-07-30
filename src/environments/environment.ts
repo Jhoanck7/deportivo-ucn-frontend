a1f86b3942d266e1261b6b990c0c4163a84e553b
Jhoan Montero <jhoan.montero@alumnos.ucn.cl>
2026-07-30 18:28:23 -0400

    feat(env): add Google client ID config values

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:5059/api'
+    apiUrl: 'http://localhost:5059/api',
+    googleClientId: '890308884774-eifs9hbd1t0r1tobbpek1jfsl3tumqah.apps.googleusercontent.com'
 };
